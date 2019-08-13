9a5b955088ff238e96fe2e9f89db98f8f8da7fa9
nsassis <54067145+nsassis@users.noreply.github.com>
2019-08-13 11:15:59 -0300

    Update the code error.jl

--- the code error.jl
+++ the code error.jl
@@ -1,3 +1,22 @@
+##############################################################################################################
+Nícolas Samuel Assis
+Universidade Estadual Paulista 
+Copyright 1989, 2004, 2005, 2009, 2016, 2019 Socorro Rangel/UNESP
+This program is part of CorteBiFur
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################################################
+
 using JuMP
 
 ######################### This is the method, not complete, but here is the problem #######################
